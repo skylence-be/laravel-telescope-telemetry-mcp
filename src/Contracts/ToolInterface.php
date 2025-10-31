7ca7d5eac14969b32dc2dae1b416e03b12079de9
Jonas Vanderhaegen <jonas@xve.be>
2025-10-31 08:21:15 +0100

    Fix ToolInterface to use getShortName() and getSchema() methods

--- src/Contracts/ToolInterface.php
+++ src/Contracts/ToolInterface.php
@@ -1,44 +1,48 @@
 <?php
 
-namespace Skylence\\TelescopeMcp\Contracts;
+declare(strict_types=1);
+
+namespace Skylence\TelescopeMcp\Contracts;
 
 interface ToolInterface
 {
     /**
-     * Get the tool name.
+     * Get the tool's short name (used as identifier).
+     */
+    public function getShortName(): string;
+
+    /**
+     * Get the tool's full name.
      */
     public function getName(): string;
-    
+
     /**
-     * Get the tool description.
+     * Get the tool's schema definition.
+     *
+     * @return array{name: string, description: string, inputSchema: array}
      */
-    public function getDescription(): string;
-    
-    /**
-     * Get the tool's input schema.
-     */
-    public function getInputSchema(): array;
-    
+    public function getSchema(): array;
+
     /**
      * Execute the tool with given arguments.
      */
     public function execute(array $arguments = []): array;
-    
+
     /**
      * Get summary view of the data.
      */
     public function summary(array $arguments = []): array;
-    
+
     /**
      * Get list view of the data.
      */
     public function list(array $arguments = []): array;
-    
+
     /**
      * Get detailed view of a single item.
      */
     public function detail(string $id, array $arguments = []): array;
-    
+
     /**
      * Get statistics about the data.
      */
